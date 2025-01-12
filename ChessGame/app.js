b4f2cf93599937b0bb985f1a6e03d197924b0073
Manojkumar9043G <manojvasanth57986@gmail.com>
2025-01-12 17:28:44 +0530

    New Change

--- ChessGame/app.js
+++ ChessGame/app.js
@@ -1,34 +1,72 @@
-const GameBored = document.querySelector('#GameBored');
+const ChessBored = document.querySelector('#GameBored');
 
-const GamePieces = [
+console.log(ChessBored);
+
+const Pieces = [
     Rook,Knight,Bishop,queen,king,Bishop,Knight,Rook,
     Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,
     '','','','','','','','',
     '','','','','','','','',
     '','','','','','','','',
     '','','','','','','','',
-    Rook,Knight,Bishop,queen,king,Bishop,Knight,Rook,
-    Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn
+    Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,Bawn,
+    Rook,Knight,Bishop,queen,king,Bishop,Knight,Rook
 ];
 
-GamePieces.forEach((GamePieces,i)=>{
-    const pieces = document.createElement('div');
-    pieces.classList.add('pieces');
-    pieces.setAttribute('Picese-id',i);
 
-    const row = Math.floor((63-i)/8)+1;
-    if(row % 2 === 0){
-        pieces.classList.add(i%2==0 ? 'red':'white');
-    }
-    else{
-        pieces.classList.add(i%2==0 ? 'white':'red');
-    }
 
-    pieces.innerHTML = GamePieces;
-    GameBored.appendChild(pieces);
+    Pieces.forEach((piece,i)=>{
+        const ChessP = document.createElement('div');
+        ChessP.setAttribute('Piece-id',i);
+        ChessP.setAttribute('draggable',true);
+        ChessP.classList.add('pieceBack');
 
-    if(i <=15){
-        const colorOfP = pieces.firstChild.firstChild
-        colorOfP.classList.add('white');
-    }
-});
+        let row = Math.floor((63-i)/8)+1;
+
+        if(row%2==0){
+            ChessP.classList.add(i%2==0 ? 'blue':'green');
+        }
+        else{
+            ChessP.classList.add(i%2==0 ? 'green':'blue');
+        }
+
+        ChessP.innerHTML = piece;
+        ChessBored.append(ChessP);
+
+        if(i <=15){
+            ChessP.firstChild.classList.add('white');
+        }
+
+        if(i>=48){
+    
+            ChessP.classList.add('black');
+        }
+    })
+
+const MoveElement = document.querySelectorAll('.pieceBack');
+
+MoveElement.forEach((Pieces)=>{
+    Pieces.addEventListener('dragstart',DragStart);
+    Pieces.addEventListener('dragover',DragOver);
+    Pieces.addEventListener('drop',DropPicec);
+})
+
+
+
+let ElementPiece;
+let ElementId;
+function DragStart(event){
+    ElementPiece = event.target.firstChild;
+    console.log(ElementPiece);
+};
+
+function DragOver(event){
+    event.preventDefault();
+}
+
+function DropPicec(event){
+    
+    event.target.firstChild.remove();
+    event.target.append(ElementPiece);
+    // console.log(event.target)
+}
